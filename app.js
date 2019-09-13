568c1186a99c13130a66732ca48962cb98ca1570
Blaise Njike <chukwudinjike17@gmail.com>
2019-09-13 11:12:10 +0100

    My Wordpress Hand Coded Site

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("Hello thanks for checking me out :) this site is stil under development");
+alert("Click on the first link to show how post looks like ");
+alert("Please best viewed on mobie, desktop hasent been optimized yet...... ");
+alert("Please not that this a coded replica of the original made with Wordpress https://afrojac10.com click on logo to take you to original")
